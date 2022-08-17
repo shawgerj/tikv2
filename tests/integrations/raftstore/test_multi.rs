f796430d30a394499634801a4b723486bb296d10
John Shawger <shawgerj@cs.wisc.edu>
2022-08-17 12:37:50 -0500

    improved tests

--- tests/integrations/raftstore/test_multi.rs
+++ tests/integrations/raftstore/test_multi.rs
@@ -916,38 +916,10 @@ fn test_cluster_restart_normal() {
     must_get_equal(&cluster.get_engine(1), b"0009", b"0009");
 }
 
-#[test]
-fn test_cluster_wal_sync_fault() {
-    let mut cluster = new_fault_node_cluster(0, 1);
-    cluster.cfg.raft_store.raft_base_tick_interval = ReadableDuration::millis(500);
-    cluster.cfg.raft_store.raft_max_size_per_msg = ReadableSize(5);
-    cluster.cfg.raft_store.raft_election_timeout_ticks = 50;
-    cluster.cfg.raft_store.max_leader_missing_duration = ReadableDuration::hours(1);
-    cluster.cfg.raft_store.peer_stale_state_check_interval = ReadableDuration::minutes(30);
-    cluster.cfg.raft_store.abnormal_leader_missing_duration = ReadableDuration::hours(1);
-    // disable compact log to make test more stable.
-    cluster.cfg.raft_store.raft_log_gc_threshold = 3000;
-    cluster.pd_client.disable_default_operator();
-
-    cluster.run_conf_change();
-
-    for i in 0..5 {
-        let v = format!("{:04}", i);
-        cluster.async_put(v.as_bytes(), v.as_bytes()).unwrap();
-    }
-    must_get_equal(&cluster.get_engine(1), b"0004", b"0004");
-
-    sleep_ms(500);    
-    cluster.stop_node(1);
-    cluster.reboot_engine(1);
-    cluster.run_node(1).unwrap();
-        
-//    cluster.stop_node(1);
-//    cluster.run_node(1).unwrap();
-    must_get_none(&cluster.get_engine(1), b"0004");
-}
-
-
+/* this test uses the failwrite rocksdb option added by shawgerj to lose
+ * unflushed memtable data when the kv engine is restarted. It is meant to
+ * simulate data lost in a power failure.
+ */ 
 #[test]
 fn test_cluster_restart_failwrite() {
     let mut cluster = new_fault_node_cluster(0, 1);
@@ -960,26 +932,30 @@ fn test_cluster_restart_failwrite() {
     // disable compact log to make test more stable.
     cluster.cfg.raft_store.raft_log_gc_threshold = 3000;
     cluster.pd_client.disable_default_operator();
-
     cluster.run_conf_change();
 
-    cluster.must_put(b"k1", b"v1");
     for i in 0..10 {
         let v = format!("{:04}", i);
-        cluster.async_put(v.as_bytes(), v.as_bytes()).unwrap();
+        cluster.must_put(v.as_bytes(), v.as_bytes());
     }
+    must_get_equal(&cluster.get_engine(1), b"0001", b"0001");
     must_get_equal(&cluster.get_engine(1), b"0009", b"0009");
 
-    sleep_ms(500);    
     cluster.stop_node(1);
-    cluster.reboot_engine(1);
-    cluster.run_node(1).unwrap();
-        
-//    cluster.stop_node(1);
-//    cluster.run_node(1).unwrap();
+    cluster.reboot_engine(1); // not flushed: we lose data here
+    must_get_none(&cluster.get_engine(1), b"0001");
+    must_get_none(&cluster.get_engine(1), b"0009");
+    cluster.run_node(1).unwrap(); // reapply raft log on startup
+    sleep_ms(2000); // race to catch up raft log before reading from engine... 
+
+    must_get_equal(&cluster.get_engine(1), b"0001", b"0001");
     must_get_equal(&cluster.get_engine(1), b"0009", b"0009");
 }
 
+/* this writes k1 and k2 to the cluster, then manually loses data by resetting
+ * ApplyState to an earlier index, and deleting k2. k2 should be found again
+ * after restarting the node, because the raft log will be reapplied
+ */
 #[test]
 fn test_cluster_restart_reapply_raft_log() {
     // setup and run 1-node cluster
