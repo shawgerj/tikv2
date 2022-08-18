1bf69b0e49b4f49ea7c569c427a28634eb3b0a89
John Shawger <shawgerj@cs.wisc.edu>
2022-08-18 12:55:12 -0500

    more write_to_db failpoint tests

--- tests/failpoints/cases/test_write_to_db.rs
+++ tests/failpoints/cases/test_write_to_db.rs
@@ -7,8 +7,7 @@ use test_raftstore::*;
 use tikv_util::config::*;
 use tikv_util::HandyRwLock;
 
-#[test]
-fn test_fail_write_to_db_begin() {
+fn run_write_to_db_with_failpoint(fp: &str) {
     let mut cluster = new_fault_node_cluster(0, 1);
     cluster.cfg.raft_store.raft_base_tick_interval = ReadableDuration::millis(500);
     cluster.cfg.raft_store.raft_max_size_per_msg = ReadableSize(5);
@@ -21,8 +20,7 @@ fn test_fail_write_to_db_begin() {
     cluster.pd_client.disable_default_operator();
     cluster.run_conf_change();
 
-    let apply_fp = "write_to_db_begin";
-    fail::cfg(apply_fp, "return").unwrap();
+    fail::cfg(fp, "return").unwrap();
 
     for i in 0..10 {
         let v = format!("{:04}", i);
@@ -35,10 +33,35 @@ fn test_fail_write_to_db_begin() {
     cluster.reboot_engine(1);
     must_get_none(&cluster.get_engine(1), b"0001");
     must_get_none(&cluster.get_engine(1), b"0009");
-    fail::remove(apply_fp);
+    fail::remove(fp);
     cluster.run_node(1).unwrap(); // reapply raft log on startup
     sleep_ms(2000); // race to catch up raft log before reading from engine...
 
     must_get_equal(&cluster.get_engine(1), b"0001", b"0001");
     must_get_equal(&cluster.get_engine(1), b"0009", b"0009");
+}
+
+#[test]
+fn test_fail_write_to_db_begin() {
+    run_write_to_db_with_failpoint("write_to_db_begin");
+}
+
+#[test]
+fn test_fail_write_to_db_after_ingest_sst() {
+    run_write_to_db_with_failpoint("write_to_db_after_ingest_sst");
+}
+
+#[test]
+fn test_fail_write_to_db_after_write_wb() {
+    run_write_to_db_with_failpoint("write_to_db_after_write_wb");
+}
+
+#[test]
+fn test_fail_write_to_db_after_sync_memtables() {
+    run_write_to_db_with_failpoint("write_to_db_after_sync_memtables");
+}
+
+#[test]
+fn test_fail_write_to_db_after_delete_ssts() {
+    run_write_to_db_with_failpoint("write_to_db_after_delete_ssts");
 }
